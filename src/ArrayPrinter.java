10a9927d052921a33e89571fadb2878b088268a5
CreoComp <q2e4563@gmail.com>
2024-09-10 03:30:59 +0300

    Initial commit

---
+++ src/ArrayPrinter.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ArrayPrinter {
+
+    public static String printArray(Object[] array)
+    {
+        return Arrays.stream(array).map(Object::toString).collect(Collectors.joining(","));
+    }
+}
